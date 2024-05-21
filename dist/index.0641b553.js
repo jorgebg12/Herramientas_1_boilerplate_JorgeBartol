94fa0635ed543b08cfd174cd21859b4f6bccc17b
jorgebg12 <jorgebartol12@gmail.com>
2024-05-21 19:10:23 +0200

    Pec 2 home clip-path

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -584,8 +584,27 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"bNKaB":[function(require,module,exports) {
-const name = "world";
-console.log(`Hello ${name}`);
+const contenedor = document.getElementById("clip-home-container");
+const imagen = document.getElementById("cliping-img");
+contenedor.addEventListener("mousemove", function(event) {
+    imagen.style.animation = "";
+    const rectContainer = contenedor.getBoundingClientRect();
+    const rectImage = imagen.getBoundingClientRect();
+    const mouseXPercent = (event.clientX - rectImage.left) / rectImage.width * 100;
+    const link = document.elementFromPoint(event.clientX, event.clientY);
+    if (link.className == "home-link") {
+        const dimensionsLink = link.getBoundingClientRect();
+        const relativeX = dimensionsLink.left - rectContainer.left;
+        const updatedClipPath = `polygon(${relativeX - 30}px 0% ,${relativeX + dimensionsLink.width + 30}px  0%, 50% ${rectContainer.height}px)`;
+        imagen.style.clipPath = updatedClipPath;
+    } else {
+        const updatedClipPath = `polygon(${mouseXPercent - 10}% 0% ,${mouseXPercent + 10}%  0%, 50% ${rectContainer.height}px)`;
+        imagen.style.clipPath = updatedClipPath;
+    }
+});
+contenedor.addEventListener("mouseleave", function(event) {
+    imagen.style.animation = "clip 10s infinite";
+});
 
 },{}]},["gbXMy","bNKaB"], "bNKaB", "parcelRequire9b8f")
 
